cf9fb29da5a07c25f01896a2777a235e6324f349
xyzmean <163691714+xyzmean@users.noreply.github.com>
2026-09-26 13:11:24 +0300

    products/der_cf_x86_64_phone: исключения разбора Soong как у Lineage
    Без них prebuilts/misc/protobuf_vendorcompat и hardware/lineage/compat определяют одни и те
    же библиотеки, и Soong не собирает дерево.

--- products/der_cf_x86_64_phone.mk
+++ products/der_cf_x86_64_phone.mk
@@ -5,3 +5,9 @@ $(call inherit-product, vendor/der/config/der.mk)
 PRODUCT_NAME := der_cf_x86_64_phone
 PRODUCT_DEVICE := vsoc_x86_64_only
 PRODUCT_MODEL := Cuttlefish x86_64 phone (der-exp)
+
+# Каталоги, которые Lineage исключает из разбора Soong в vendor/lineage/config/common.mk: продукт
+# AOSP этот файл не наследует, а без исключения protobuf_vendorcompat определён дважды (здесь и
+# в hardware/lineage/compat) и Soong отказывается собирать дерево.
+PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
+PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
